5104d7db135aa21dbb4c2baa37d4ae6f18ed3b3c
Carlos del Águila <xolcar@gmail.com>
2024-10-23 18:47:12 +0200

    added files to create RADIs

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Default branch if not specified
-ROBOTICS_ACADEMY="master"
-ROBOTICS_INFRASTRUCTURE="noetic-devel"
-RAM="main"
-ROS_DISTRO="noetic"
+ROBOTICS_ACADEMY="humble-devel"
+ROBOTICS_INFRASTRUCTURE="humble-devel"
+RAM="humble-devel"
+ROS_DISTRO="humble"
 IMAGE_TAG="test"
 FORCE_BUILD=false
+FORCE_BUILD_NO_CACHE=false
 
 Help()
 {
@@ -16,10 +17,11 @@ Help()
    echo "  -h                        Print this Help."
    echo "  -f                        Force creation of the base image. If omitted, the base image is created only if "
    echo "                            it doesn't exist."
-   echo "  -a, --academy    <value>  Branch of RoboticsAcademy.               Default: master"
-   echo "  -i, --infra      <value>  Branch of RoboticsInfrastructure.        Default: noetic-devel"
-   echo "  -m, --ram        <value>  Branch of RoboticsApplicationManager.    Default: main"
-   echo "  -r, --ros        <value>  ROS Distro (humble or noetic).           Default: noetic"
+   echo "  -F                        Force creation of the base image without using docker cache."
+   echo "  -a, --academy    <value>  Branch of RoboticsAcademy.               Default: humble-devel"
+   echo "  -i, --infra      <value>  Branch of RoboticsInfrastructure.        Default: humble-devel"
+   echo "  -m, --ram        <value>  Branch of RoboticsApplicationManager.    Default: humble-devel"
+   echo "  -r, --ros        <value>  ROS Distro (humble or noetic).           Default: humble"
    echo "  -t, --tag        <value>  Tag name of the image.                   Default: test"
    echo
    echo "Example:"
@@ -55,8 +57,12 @@ while [[ $# -gt 0 ]]; do
             FORCE_BUILD=true
             shift
             ;;
+        -F | --force-no-cache)
+            FORCE_BUILD_NO_CACHE=true
+            shift
+            ;;
         -h | --help) # display Help
-            echo "Generates RoboticsAcademy RADI image"
+            echo "Generates RoboticsAcademy RoboticsBackend image"
             echo
             Help
             exit 0
@@ -79,25 +85,38 @@ echo
 # Determine Dockerfile based on ROS_DISTRO
 if [[ $ROS_DISTRO == "noetic" ]]; then
     DOCKERFILE_BASE="Dockerfile.dependencies_noetic"
-    DOCKERFILE="Dockerfile.noetic"
+    DOCKERFILE="Dockerfile.mini_noetic"
 elif [[ $ROS_DISTRO == "humble" ]]; then
     DOCKERFILE_BASE="Dockerfile.dependencies_humble"
-    DOCKERFILE="Dockerfile.humble"
+    DOCKERFILE="Dockerfile.mini_humble"
 else
     echo "Error: Unknown ROS_DISTRO ($ROS_DISTRO). Please set it to 'noetic' or 'humble'."
     exit 1
 fi
 
+if $FORCE_BUILD_NO_CACHE; then
+  NO_CACHE="--no-cache"
+else
+  NO_CACHE=""
+fi
+
 # Build the Docker Base image
-if $FORCE_BUILD || [[ "$(docker images -q jderobot/robotics-applications:dependencies-$ROS_DISTRO 2> /dev/null)" == "" ]]; then
+if $FORCE_BUILD_NO_CACHE || $FORCE_BUILD || [[ "$(docker images -q jderobot/robotics-applications:dependencies-$ROS_DISTRO 2> /dev/null)" == "" ]]; then
   echo "===================== BUILDING $ROS_DISTRO BASE IMAGE ====================="
   echo "Building base using $DOCKERFILE_BASE for ROS $ROS_DISTRO"
-  docker build -f $DOCKERFILE_BASE -t jderobot/robotics-applications:dependencies-$ROS_DISTRO .
+  docker build $NO_CACHE -f $DOCKERFILE_BASE -t jderobot/robotics-applications:dependencies-$ROS_DISTRO .
+fi
+
+if [ $? -eq 0 ]; then
+    echo "Docker Base Image Build Successful"
+else
+    echo "Docker Base Image Build FAILED...exiting"
+    exit
 fi
 
 # Build the Docker image
-echo "===================== BUILDING $ROS_DISTRO RADI ====================="
-echo "Building RADI using $DOCKERFILE for ROS $ROS_DISTRO"
+echo "===================== BUILDING $ROS_DISTRO RoboticsBackend ====================="
+echo "Building RoboticsBackend using $DOCKERFILE for ROS $ROS_DISTRO"
 
 docker build --no-cache -f $DOCKERFILE \
   --build-arg ROBOTICS_ACADEMY=$ROBOTICS_ACADEMY \
@@ -105,4 +124,4 @@ docker build --no-cache -f $DOCKERFILE \
   --build-arg RAM=$RAM \
   --build-arg ROS_DISTRO=$ROS_DISTRO \
   --build-arg IMAGE_TAG=$IMAGE_TAG \
-  -t jderobot/robotics-academy:$IMAGE_TAG .
+  -t jderobot/robotics-backend:$IMAGE_TAG .
